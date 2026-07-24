1c484d3666d74cf5d49179f6d8799f19f063513e
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-07-24 23:21:41 +0530

    updated mongoose.connect() in app.js

--- app.js
+++ app.js
@@ -13,19 +13,20 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-try {
 console.log("URI:", process.env.MONGO_URI);
 console.log("USER:", process.env.MONGO_USERNAME);
 console.log("PASS exists:", !!process.env.MONGO_PASSWORD);
-    await mongoose.connect(process.env.MONGO_URI, {
-        user: process.env.MONGO_USERNAME,
-        pass: process.env.MONGO_PASSWORD
-    });
 
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD
+})
+.then(() => {
     console.log("MongoDB Connected");
-} catch (err) {
-    console.error(err);
-}
+})
+.catch((err) => {
+    console.error("MongoDB Connection Error:", err);
+});
 
 var Schema = mongoose.Schema;
 
